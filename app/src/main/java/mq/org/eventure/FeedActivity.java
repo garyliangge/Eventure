4e4b20adfa29d8be6ff39b1d530c8162dbc766c1
garyliangge <garyliangge@berkeley.edu>
2016-03-05 15:37:52 -0800

    Basic list view for feed

--- app/src/main/java/mq/org/eventure/FeedActivity.java
+++ app/src/main/java/mq/org/eventure/FeedActivity.java
@@ -1,24 +1,59 @@
 package mq.org.eventure;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import android.widget.AdapterView.OnItemClickListener;
+
 
 /**
  * Created by Michael on 2/18/16.
  */
-public class FeedActivity extends AppCompatActivity {
+public class FeedActivity extends ListActivity {
+
+
+
+
+
+
+    static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+
+        // we dont need this
+        // setContentView(R.layout.list_fruit);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_feed, R.id.eventTextView, FRUITS));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    private void openMap(){
-        Intent intent = new Intent(this, MapActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.move_in_right, R.anim.move_out_right);
-        finish();
-    }
+
+
 }
