3581c1b839b128cfdaaad724772ec57583f9328c
Michael <mzqu98>
2016-03-05 15:36:48 -0800

    Activity background color messing up, fixed that. Map has slider and can change radius of a circle on the map. You can see your location now on the map.

--- app/src/main/java/mq/org/eventure/MapActivity.java
+++ app/src/main/java/mq/org/eventure/MapActivity.java
@@ -1,55 +1,95 @@
 package mq.org.eventure;
 
-import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+public class MapActivity extends AppCompatActivity {
+
+    LatLng loc;
+    double minDistance, maxDistance, distance; //IN METERS
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        minDistance = 1000;
+        maxDistance = 10000;
+
+        distance = minDistance;
+        ((TextView)findViewById(R.id.currentRadius)).setText(distance + " meter radius");
+
+        //replace GOOGLE MAP fragment in this Activity
+        replaceMapFragment();
+
+        ((SeekBar)findViewById(R.id.seekBar1)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    distance = 1000 + ((maxDistance - minDistance) / 9) * progress;
+                    ((TextView)findViewById(R.id.currentRadius)).setText(distance + " meter radius");
+                    redraw();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
-    private void openFeed(){
-        Intent intent = new Intent(this, FeedActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.move_in_left, R.anim.move_out_left);
-        finish();
+    private void replaceMapFragment() {
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
 
+        // Enable Zoom
+        map.getUiSettings().setZoomGesturesEnabled(true);
+
+        //set Map TYPE
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //enable Current location Button
+        map.setMyLocationEnabled(true);
+
+        //set "listener" for changing my location
+        map.setOnMyLocationChangeListener(myLocationChangeListener());
     }
 
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener() {
+        return new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                loc = new LatLng(location.getLatitude(), location.getLongitude());
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+                redraw();
+            }
+        };
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void redraw(){
+        if (map != null && loc != null) {
+            map.clear();
+
+            map.addCircle(new CircleOptions()
+                    .center(loc)
+                    .radius(distance)
+                    .strokeColor(Color.parseColor("#A8E9FF"))
+                    .fillColor(Color.parseColor("#4DA8E9FF")));
+        }
     }
 }
